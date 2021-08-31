4eeae53a9f8039626efe5496efd6dc4b2554228d
immingmar <immingmar@gmail.com>
2021-08-31 17:10:17 +0545

    Share-Button added

---
+++ index.js
@@ -0,0 +1,7 @@
+
+var popup = document.querySelector(".popup");
+document.querySelector(".share-btn").addEventListener("click",shareButton);
+
+function shareButton() {
+    popup.classList.toggle("show");
+}
